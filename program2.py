4342a76c0141b48600172786c3929b7a657e9933
Mohit Nare <mohitnare13@gail.com>
2024-09-27 13:55:05 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -3,6 +3,9 @@ class Solution(object):
        roman_values = {
            "I":1 , "V" :5 , "X":10 ,"L":50 , "C":100 , "D":500 ,"M":1000
        }
+       toatl = 0
+       prev_value = 0
+       for char in reversed(str):
 
 
 
