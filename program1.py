02242bd5484594083d21320aa0c2035d953eec9c
Mohit Nare <mohitnare13@gail.com>
2024-09-27 19:05:04 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -1,27 +1,27 @@
 class Solution:
-    def isValid(self, s: str) -> bool:
-
-        bracket_map = {')': '(', ']': '[', '}': '{'}
-        stack = []
-
+    def romanToInt(self, s: str) -> int:
+        # Roman numerals mapping
+        roman_to_int = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
         
-        for char in s:
-            if char in bracket_map:  
-                
-                top_element = stack.pop() if stack else '#'
-                
-               
-                if bracket_map[char] != top_element:
-                    return False
+        total = 0
+        prev_value = 0
+
+        # Traverse the Roman numeral from right to left
+        for char in reversed(s):
+            current_value = roman_to_int[char]
+            # If the current value is less than the previous value, it means we need to subtract
+            if current_value < prev_value:
+                total -= current_value
             else:
-                
-                stack.append(char)
-
-       
-        return not stack
-
-# Example usage
-validator = Solution()
-print(validator.isValid("()"))       
-print(validator.isValid("()[]{}"))   
-print(validator.isValid("(]"))  
+                total += current_value
+            prev_value = current_value
+        
+        return total
