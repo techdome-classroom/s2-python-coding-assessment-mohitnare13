dfde219c6638c509da9de4a70c189a372446ecbd
Mohit Nare <mohitnare13@gail.com>
2024-09-27 18:43:05 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -3,7 +3,7 @@ class Solution:
         roman_values = {
            'I': 1, 'V': 5, 'X': 10, 'L': 50, 
            'C': 100, 'D': 500, 'M': 1000
-    }
+        }
 
     total = 0
     prev_value = 0
