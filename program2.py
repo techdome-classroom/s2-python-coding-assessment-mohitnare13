f2ffd04372a495ac0921070120680de821072083
Mohit Nare <mohitnare13@gail.com>
2024-09-27 18:42:43 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -1,4 +1,5 @@
-def romanToInt(s: str) -> int:
+class Solution:
+    def romanToInt(s: str) -> int:
     roman_values = {
         'I': 1, 'V': 5, 'X': 10, 'L': 50, 
         'C': 100, 'D': 500, 'M': 1000
