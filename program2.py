ae072e0a8a17ae38ccaee9be961fdcaf558ab687
Mohit Nare <mohitnare13@gail.com>
2024-09-27 13:55:57 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -1,11 +1,8 @@
-class Solution(object):
-    def romanToInt(self, str):
+def romanToInt(self, str):
        roman_values = {
            "I":1 , "V" :5 , "X":10 ,"L":50 , "C":100 , "D":500 ,"M":1000
        }
        toatl = 0
        prev_value = 0
        for char in reversed(str):
-
-
-
+           
