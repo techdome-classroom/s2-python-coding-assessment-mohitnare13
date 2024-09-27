9deb28ca7e315e936be553bf37574bee150aba2b
Mohit Nare <mohitnare13@gail.com>
2024-09-27 18:43:35 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -1,8 +1,7 @@
-class Solution:
-    def romanToInt(s: str) -> int:
-        roman_values = {
-           'I': 1, 'V': 5, 'X': 10, 'L': 50, 
-           'C': 100, 'D': 500, 'M': 1000
+def romanToInt(s: str) -> int:
+    roman_values = {
+    'I': 1, 'V': 5, 'X': 10, 'L': 50, 
+    'C': 100, 'D': 500, 'M': 1000
         }
 
     total = 0
