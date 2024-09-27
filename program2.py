651461e61f99016d51000b0a1180d9df7a8b3eb0
Mohit Nare <mohitnare13@gail.com>
2024-09-27 13:59:00 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -2,13 +2,13 @@ def romanToInt(str):
        roman_values = {
            "I":1 , "V" :5 , "X":10 ,"L":50 , "C":100 , "D":500 ,"M":1000
        }
-       toatl = 0
-       prev_value = 0
-       for char in reversed(str):
-           value = roman_values[char]
-           if value < prev_value :
+        toatl = 0
+        prev_value = 0
+        for char in reversed(str):
+            value = roman_values[char]
+            if value < prev_value :
                total -= value
-           else :
+            else :
               
                 total += value
             prev_value = value
