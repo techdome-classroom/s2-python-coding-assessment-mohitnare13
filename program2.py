58caf83826981cf8672aa47e80a4d9b128985702
Mohit Nare <mohitnare13@gail.com>
2024-09-27 18:58:39 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -1,22 +1,27 @@
-def romanToInt(s: str) -> int:
-    roman_values = {
-    'I': 1, 'V': 5, 'X': 10, 'L': 50, 
-    'C': 100, 'D': 500, 'M': 1000
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        # Roman numerals mapping
+        roman_to_int = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
         }
+        
+        total = 0
+        prev_value = 0
 
-    total = 0
-    prev_value = 0
-
-    for char in reversed(s):
-        value = roman_values[char]
-        if value < prev_value:
-            total -= value
-        else:
-            total += value
-        prev_value = value
-
-    return total
-
-print(romanToInt("III"))     
-print(romanToInt("LVIII"))    
-print(romanToInt("MCMXCIV"))
+        # Traverse the Roman numeral from right to left
+        for char in reversed(s):
+            current_value = roman_to_int[char]
+            # If the current value is less than the previous value, it means we need to subtract
+            if current_value < prev_value:
+                total -= current_value
+            else:
+                total += current_value
+            prev_value = current_value
+        
+        return total
