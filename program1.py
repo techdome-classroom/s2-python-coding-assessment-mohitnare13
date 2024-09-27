982c9f223953d96cf5d8aadb738b5b74ed937e3f
Mohit Nare <mohitnare13@gail.com>
2024-09-27 14:15:54 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -7,7 +7,7 @@ class Solution:
         
         for char in s:
             if char in bracket_map:  
-                # Pop the top element of the stack, if stack is not empty; otherwise, use a dummy value '#'
+                
                 top_element = stack.pop() if stack else '#'
                 
                 # If the top element doesn't match the corresponding opening bracket, return False
