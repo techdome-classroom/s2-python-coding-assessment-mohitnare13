7cccc6ee5f42b2a4e9f303ef6a7ec65866e7b4b9
Mohit Nare <mohitnare13@gail.com>
2024-09-27 14:02:13 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -1,20 +1,46 @@
-class Solution(object):
-    def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        pass
+class Solution:
+    def isValid(self, s: str) -> bool:
+        # Stack to store the opening brackets
+        stack = []
+        # Mapping of closing brackets to opening brackets
+        mapping = {')': '(', ']': '[', '}': '{'}
+        
+        # Iterate through the string
+        for char in s:
+            if char in mapping:
+                # Get the top element from stack, or a dummy value if stack is empty
+                top_element = stack.pop() if stack else '#'
+                
+                # If the mapped value of the current closing bracket doesn't match the stack's top element
+                if mapping[char] != top_element:
+                    return False
+            else:
+                stack.append(char)
+        
+        # If stack is empty, all opening brackets are closed properly
+        return not stack
 
 
+# Unit test code
+import unittest
 
+class TestSolution(unittest.TestCase):
+    def setUp(self):
+        self.solution = Solution()
 
+    def test_valid_parentheses(self):
+        self.assertTrue(self.solution.isValid("()"))
+        self.assertTrue(self.solution.isValid("()[]{}"))
+        self.assertTrue(self.solution.isValid("{[()]}"))
 
+    def test_invalid_parentheses(self):
+        self.assertFalse(self.solution.isValid("(]"))
+        self.assertFalse(self.solution.isValid("([)]"))
 
+    def test_empty_string(self):
+        self.assertTrue(self.solution.isValid(""))
+    def test_mixed_parentheses(self):
+        self.assertFalse(self.solution.isValid("(){"))
 
-    
-
-
-
-  
-
+if __name__ == '_main_':
+    unittest.main(argv=['first-arg-is-ignored'],exit=False)
